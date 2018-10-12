519a05a86d2b186c9dfc406c85c29073543289de
Manuel Lanctot <manuel@uniregistry.com>
2018-10-12 14:43:21 +0100

    split the urwid classes into window.py

---
+++ idle.py
@@ -0,0 +1,176 @@
+from random import choice, randrange
+import time
+import json
+import urwid
+from window import MainWindow
+
+#import urwid.curses_display
+
+### TODO
+# X game pause
+# antigens attacking (virus)
+# antigens replicating
+# python native 64-bit numbers?
+# keypress for bigger/longer log window
+# X programmed cell death
+# mouse clicking for manual protein production?
+# add a single option that scales down/up the game in speed (hp, seconds, defense, protein, etc.)
+
+class Game:
+    def __init__(self, data):
+        self.data = data
+        self.ticks = 0
+        self.level = 0
+        self.proteins = 0
+        self.player_cells = []
+        self.antigens = []
+        self.msglog = []
+        self.paused = False
+
+    def display_log(self):
+        return '\n'.join(self.msglog[-5:])
+
+    def log(self, str):
+        self.msglog.append(str)
+
+    def initialize_player(self):
+        # Start with a single random white cell
+        name = choice(list(self.data['white cell']))
+        c = Cell(name=name, data=self.data['white cell'][name], game=self)
+        c.spawn()
+
+        #self.player_cells.append(Cell('T helper', self.data['white cell']['T helper']))
+
+    def initialize_level(self):
+        for i in range(0, self.level):
+            name = choice(list(self.data['antigen']))
+
+            # multiply base_hp with the current level
+            a = Antigen(name=name, data=self.data['antigen'][name], game=self)
+            a.spawn()
+            a.hp = a.hp * self.level
+
+            #self.antigens.append(a)
+
+    def check_level_state(self):
+        if len(self.antigens) == 0:
+            self.level += 1
+            self.initialize_level()
+
+    def dice_roll(self, min, max):
+        total = 0
+        total += randrange(min, max + 1)
+        if total == max + 1:
+            total += self.dice_roll(min, max)
+        return total
+
+    def flash(self, loop, target):
+        target.palette = "flash %s" % target.palette
+        loop.widget.mainloop.draw_antigens()
+        loop.draw_screen()
+        loop.set_alarm_in(0.2, self.unflash, target)
+
+    def unflash(self, loop, target):
+        target.palette = target.palette.replace('flash ', '')
+        loop.widget.mainloop.draw_antigens()
+        loop.draw_screen()
+
+    def player_attack(self, mwin):
+        for c in self.player_cells:
+            if not self.antigens:
+                return
+
+            target = choice(self.antigens)
+
+            # Calculate damage
+            dmg = c.base_attack
+            dmg += self.dice_roll(1, 3) # add some randomness
+            dmg -= target.defense
+
+            if dmg > 0:
+                ### EXPERIMENTAL
+                mwin.loop.set_alarm_in(0, self.flash, target)
+                target.hp -= dmg
+
+            self.log("%s dealt %s dmg to %s." % (c.name, dmg, target.name))
+
+            if target.hp <= 0:
+                target.die('killed by %s' % c.name)
+                # Antigen dies/is absorbed
+                #self.antigens.remove(target)
+
+    def spawn_cell(self):
+        # Cost grows the more cells you have
+        base_cost = 10
+        total_cost = base_cost + len(self.player_cells) * 20
+
+        if self.proteins >= total_cost:
+            name = choice(list(self.data['white cell']))
+            c = Cell(name=name, data=self.data['white cell'][name], game=self)
+            #self.player_cells.append(c)
+            self.proteins -= total_cost
+            self.log("%s cell purchased." % c.name)
+
+    def age_all_cells(self):
+        for c in self.player_cells:
+            c.aging(1)
+
+
+class Life(object):
+    def __init__(self, name, game, data):
+        self.name = name
+        self.game = game
+        self.data = data
+        #self.spawn()
+
+    def spawn(self):
+        self.hp = self.data.get('base_hp', 1)
+        self.defense = self.data.get('defense', 0)
+        self.description = self.data.get('description', 'No description set')
+        self.symbol = self.data.get('symbol', 'o')
+        self.palette = self.data.get('palette', 'white')
+        self.base_attack = self.data.get('base_attack', 0)
+        self.lifespan = self.data.get('lifespan', 50)
+        self.age = 0
+
+        # Add ourself to the game world
+        if type(self).__name__ == 'Cell':
+            self.game.player_cells.append(self)
+        elif type(self).__name__ == 'Antigen':
+            self.game.antigens.append(self)
+        else:
+            self.game.log("Unknown %s tried to spawn." % type(self).__name__)
+
+    def die(self, msg=None):
+        if type(self).__name__ == 'Cell':
+            self.game.player_cells.remove(self)
+        elif type(self).__name__ == 'Antigen':
+            self.game.antigens.remove(self)
+        else:
+            self.game.log("Unknown %s tried to die." % type(self).__name__)
+        if msg:
+            self.game.log("%s died %s." % (self.name, msg))
+
+    def aging(self, amt=1):
+        self.age += amt
+        if self.age > self.lifespan:
+            self.die('of old age')
+
+
+class Antigen(Life):
+    def __init__(self, name, game, data):
+        super().__init__(name, game, data)
+
+class Cell(Life):
+    def __init__(self, name, game, data):
+        super().__init__(name, game, data)
+
+
+if __name__ == '__main__':
+    json_fd = open('data.json', 'r')
+
+    game = Game(json.load(json_fd))
+    run = MainWindow(game)
+    run.main()
+
+
---
+++ window.py
@@ -0,0 +1,159 @@
+import urwid
+
+class MainWindow:
+    ##(name, foreground, background, mono, foreground_high, background_high)
+    palette = [
+        ('body', '', '', '', '#fff', '#000'),
+        ('footer', '', '', '', '#fff', '#000'),
+        ('header', '', '', '', '#fff', '#000'),
+        ('white', '', '', '', '#fff', '#000'),
+        ('brown', '', '', '', '#d80', '#000'),
+        ('yellow', '', '', '', '#ff0', '#000'),
+        ('red', '', '', '', '#f00', '#000'),
+        ('flash brown', '', '', 'bold', '#d80,standout', '#000'),
+        ('flash yellow', '', '', 'bold', '#ff0,standout', '#000'),
+        ('flash red', '', '', 'bold', '#f00,standout', '#000'),
+        ('light white', '', '', 'bold', '#fff', '#000'),
+        ('light blue', '', '', 'bold', '#0af', '#000'),
+        ('white blue', '', '', '', '#adf', '#000'),
+    ]
+
+    def __init__(self, game):
+        self.game = game
+
+        self.game.initialize_player()
+
+        self.header_text = urwid.Text(u"Top\nheader")
+        self.header = urwid.AttrWrap(self.header_text, 'header')
+
+        self.cell_txt = urwid.Text("")
+        self.antigen_txt = urwid.Text("")
+        self.player_col = urwid.Filler(self.cell_txt, valign='top', height='pack')
+        self.antigen_col = urwid.Filler(self.antigen_txt, valign='top', height='pack')
+        self.body = urwid.Columns([urwid.LineBox(self.player_col), urwid.LineBox(self.antigen_col)])
+
+        self.footer_text = urwid.Text(u"Bottom\nfooter")
+        self.footer = urwid.AttrWrap(self.footer_text, 'footer')
+
+        self.view = MainFrame(header=self.header, body=self.body, footer=self.footer, focus_part='body')
+        self.view.mainloop = self
+
+        self.last_alarm = None
+
+    def main(self):
+
+        #self.loop = urwid.MainLoop(self.view, self.palette, input_filter=self.input_filter) #, pop_ups=True)
+        self.loop = urwid.MainLoop(self.view, self.palette, pop_ups=True)
+        #self.loop.mwin = self
+
+        self.loop.screen.set_terminal_properties(colors=256)
+        self.loop.screen.register_palette(self.palette)
+
+        ### TODO: check that constantly adding alerts doesn't leak mem
+
+        # 1-second game tick
+        self.last_alarm = self.loop.set_alarm_in(0, callback=self.game_tick)
+        # 10-second game tick
+        #self.loop.set_alarm_in(0, callback=self.protein_gain)
+
+        self.loop.run()
+
+    def write_header(self):
+        header_txt = "Ticks: %s\nProteins: %s\nLevel: %s\n" % (self.game.ticks, self.game.proteins, self.game.level)
+        self.header_text.set_text(header_txt)
+
+    def write_footer(self):
+        footer_txt = self.game.display_log()
+        self.footer_text.set_text(footer_txt)
+
+    def draw_cells(self):
+        cell_str = []
+        for i in self.game.player_cells:
+            cell_str.append((i.palette, i.symbol))
+        if cell_str:
+            self.cell_txt.set_text(cell_str)
+
+    def draw_antigens(self):
+        antigen_str = []
+        for i in self.game.antigens:
+            antigen_str.append((i.palette, i.symbol))
+        if antigen_str:
+            self.antigen_txt.set_text(antigen_str)    
+
+    def game_tick(self, loop, user_data):
+        # So we have a single alarm object at all times?
+        self.loop.remove_alarm(self.last_alarm)
+
+        self.write_header()
+
+        self.draw_cells()
+        self.draw_antigens()
+
+        self.write_footer()
+
+        # If the game is paused, skip these events
+        if not self.game.paused:
+            # Player attacks
+            self.game.player_attack(self)
+
+            # Gain a protein
+            self.protein_gain(1)
+
+            self.game.check_level_state()
+            self.game.age_all_cells()
+            self.game.ticks += 1
+
+        self.last_alarm = self.loop.set_alarm_in(sec=1, callback=self.game_tick)
+
+    def protein_gain(self, amount=0):
+        self.game.proteins += amount
+
+class MainFrame(urwid.Frame):
+
+    def keypress(self, size, key):
+        #self.mainloop.game.log("key pressed: %s" % key)
+        if key == 'q':
+            raise urwid.ExitMainLoop()
+            #return super(OptionOverlay, self).keypress(size, key)
+        elif key == 'n':
+            self.mainloop.game.spawn_cell()
+            self.mainloop.write_header()
+        elif key == 'd':
+            #self.narf.game.log("focus: %s" % (self.focus))
+            self.purchase_pop_up()
+        #elif key == 'enter':
+        #    self.mainloop.game.log("enter pressed.")
+        elif key == ' ':
+            if self.mainloop.game.paused:
+                self.mainloop.game.log("Game unpaused.")
+                self.mainloop.game.paused = False
+            else:
+                self.mainloop.game.log("Game paused.")
+                self.mainloop.game.paused = True
+
+    def purchase_pop_up(self):
+
+        question = urwid.Text(("bold", "A pop-up!"), "center")
+        prompt = urwid.LineBox(urwid.Filler(question))
+        overlay = OptionOverlay(
+            top_w = prompt, 
+            bottom_w = self,
+            align = 'left',
+            width = 10,
+            valign = 'middle',
+            height = 6,
+            left = 5,
+            )
+        #self.mainloop.loop.widget.original_widget = self
+        self.mainloop.loop.widget = overlay
+
+
+class OptionOverlay(urwid.Overlay):
+    def selectable(self):
+        return True
+
+    def keypress(self, size, key):
+        if key == 'q':
+            raise urwid.ExitMainLoop()
+        elif key == 'enter':
+            self.bottom_w.mainloop.loop.widget = self.bottom_w
